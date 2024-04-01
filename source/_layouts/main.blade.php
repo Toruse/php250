aa55d014c91eb452b5efc0b5beb0f01252c0fa6c
Torus <pvne000@gmail.com>
2024-04-01 16:01:01 +0300

    Add analytics.

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YQSZJPHHB8"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YQSZJPHHB8');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="canonical" href="{{ $page->getUrl() }}">
